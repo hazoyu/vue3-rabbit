be6ee15bf971985c2459adcd93067d5a6ce4a392
hazoyu <2080686540@qq.com>
2024-06-08 10:36:42 +0800

    SKU组件认识

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import DetailHot from './components/DetailHot.vue';
 import ImageView from '@/components/ImageView/index.vue'
+import XtxSku from '@/components/XtxSku/index.vue'
 const goods=ref({})
 const route=useRoute()
 const getGoods= async() => {
@@ -12,6 +13,10 @@ const getGoods= async() => {
 }
 onMounted(()=>getGoods())
 
+//sku规格被操作时
+const skuChange=(sku)=>{
+  console.log(sku);
+}
 
 </script>
 
@@ -90,7 +95,7 @@ onMounted(()=>getGoods())
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
