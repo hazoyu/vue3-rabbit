70c1dff3bc159235be23c3466d72fb7587a18291
hazoyu <2080686540@qq.com>
2024-06-09 21:05:58 +0800

    结算页-提交订单

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -5,4 +5,13 @@ export const getCheckInfoAPI = () => {
     return request({
       url:'/member/order/pre'
     })
-  }
+}
+
+//创建订单
+export const createOrderAPI=(data)=>{
+  return request({
+    url:'/member/order',
+    method:'POST',
+    data
+  })
+}
