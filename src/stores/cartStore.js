f3626252f0585bd256607cd478fc7ad440f983eb
hazoyu <2080686540@qq.com>
2024-06-09 11:21:55 +0800

    本地购物车-列表购物车-全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -39,19 +39,32 @@ export const useCartStore=  defineStore('cart',()=>{
         item.selected=selected
     }
 
+    //全选功能
+    const allCheck=(selected)=>{
+        //把cartList中的每一项的selected都设置为当前的全选框状态
+        //forEach遍历
+        cartList.value.forEach(item=>item.selected=selected)
+    }
+
     //计算属性
     //1.总的数量
     //reduce累积操作  a=a+c.count  0初始值
     const allCount= computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
     //2.总价
     const allPrice= computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
+    
+    //是否全选 every所有项
+    const isAll=computed(()=>cartList.value.every((item)=>item.selected))
+    
     return {
-        cartList,
         addCart,
         delCart,
+        allCheck,
+        singleCheck,
         allCount,
         allPrice,
-        singleCheck
+        cartList,
+        isAll
     }
 },
 {
