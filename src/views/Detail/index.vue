9361833ee76fff3782894bd5da5d2fc0b56bb480
hazoyu <2080686540@qq.com>
2024-06-09 16:13:36 +0800

    接口购物车-加入购物车实现

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -34,7 +34,7 @@ const addCart=()=>{
     cartStore.addCart({
       id:goods.value.id,
       name:goods.value.name,
-      pictures:goods.value.mainPictures[0],
+      picture:goods.value.mainPictures[0],
       price:goods.value.price,
       count:count.value,
       skuId:skuObj.skuId,
@@ -154,7 +154,7 @@ const addCart=()=>{
                     </li>
                   </ul>
                   <!-- 图片 -->
-                  <img v-for="img in goods.details.pictures" :src="img" :key="img" alt="">
+                  <img v-for="img in goods.details.picture" :src="img" :key="img" alt="">
                 </div>
               </div>
             </div>
